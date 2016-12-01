ef9d80c140a27f34059a37d2cb47de47e940e28d
junjundeyuanwang <515334730@qq.com>
2016-12-01 12:41:55 +0800

    提交所有java类

--- adaptionsjjc/build/generated/source/dataBinding/debug/android/databinding/layouts/DataBindingInfo.java
+++ adaptionsjjc/build/generated/source/dataBinding/debug/android/databinding/layouts/DataBindingInfo.java
@@ -2,5 +2,5 @@ package android.databinding.layouts;
 
 import android.databinding.BindingBuildInfo;
 
-@BindingBuildInfo(buildId="d0633481-0c2e-4b98-bcbd-ba07a416f8cf", modulePackage="com.cnksi.sjjc", sdkRoot="D:\\eclipse\\sdk",layoutInfoDir="D:\\sjjc_android\\adaptionsjjc\\build\\intermediates\\data-binding-info\\debug",exportClassListTo="",isLibrary=false,minSdk=18,enableDebugLogs=false,printEncodedError=false)
+@BindingBuildInfo(buildId="d2a144d0-8b2d-4b85-b84f-f3ad9de71b0f", modulePackage="com.cnksi.sjjc", sdkRoot="D:\\eclipse\\sdk",layoutInfoDir="D:\\sjjc_android\\adaptionsjjc\\build\\intermediates\\data-binding-info\\debug",exportClassListTo="",isLibrary=false,minSdk=18,enableDebugLogs=false,printEncodedError=true)
 public class DataBindingInfo {}
--- adaptionsjjc/build/generated/source/dataBinding/release/android/databinding/layouts/DataBindingInfo.java
+++ adaptionsjjc/build/generated/source/dataBinding/release/android/databinding/layouts/DataBindingInfo.java
@@ -2,5 +2,5 @@ package android.databinding.layouts;
 
 import android.databinding.BindingBuildInfo;
 
-@BindingBuildInfo(buildId="de738656-f833-4e2a-8735-eeb3f5caa70d", modulePackage="com.cnksi.sjjc", sdkRoot="D:\\eclipse\\sdk",layoutInfoDir="D:\\sjjc_android\\adaptionsjjc\\build\\intermediates\\data-binding-info\\release",exportClassListTo="",isLibrary=false,minSdk=18,enableDebugLogs=false,printEncodedError=false)
+@BindingBuildInfo(buildId="a5a99097-228f-45bb-9759-8935979f4081", modulePackage="com.cnksi.sjjc", sdkRoot="D:\\eclipse\\sdk",layoutInfoDir="D:\\sjjc_android\\adaptionsjjc\\build\\intermediates\\data-binding-info\\release",exportClassListTo="",isLibrary=false,minSdk=18,enableDebugLogs=false,printEncodedError=false)
 public class DataBindingInfo {}
