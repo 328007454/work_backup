bde115ad0c327e5a330419cc1c76d1e302948d9f
junjundeyuanwang <515334730@qq.com>
2016-12-01 13:12:36 +0800

    提交忽略文件

--- adaptionsjjc/build/generated/source/dataBinding/release/android/databinding/layouts/DataBindingInfo.java
+++ adaptionsjjc/build/generated/source/dataBinding/release/android/databinding/layouts/DataBindingInfo.java
@@ -2,5 +2,5 @@ package android.databinding.layouts;
 
 import android.databinding.BindingBuildInfo;
 
-@BindingBuildInfo(buildId="a5a99097-228f-45bb-9759-8935979f4081", modulePackage="com.cnksi.sjjc", sdkRoot="D:\\eclipse\\sdk",layoutInfoDir="D:\\sjjc_android\\adaptionsjjc\\build\\intermediates\\data-binding-info\\release",exportClassListTo="",isLibrary=false,minSdk=18,enableDebugLogs=false,printEncodedError=false)
+@BindingBuildInfo(buildId="b1f9facd-2c25-438d-869e-4dc04b10a883", modulePackage="com.cnksi.sjjc", sdkRoot="D:\\eclipse\\sdk",layoutInfoDir="D:\\sjjc_android\\adaptionsjjc\\build\\intermediates\\data-binding-info\\release",exportClassListTo="",isLibrary=false,minSdk=18,enableDebugLogs=false,printEncodedError=false)
 public class DataBindingInfo {}
