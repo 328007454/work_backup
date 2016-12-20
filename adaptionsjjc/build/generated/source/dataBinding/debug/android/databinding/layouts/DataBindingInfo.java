5efa467baa7b367c08106ff0760117a6dbe39418
junjundeyuanwang <515334730@qq.com>
2016-12-20 11:33:59 +0800

    nothing changes

--- adaptionsjjc/build/generated/source/dataBinding/debug/android/databinding/layouts/DataBindingInfo.java
+++ adaptionsjjc/build/generated/source/dataBinding/debug/android/databinding/layouts/DataBindingInfo.java
@@ -2,5 +2,5 @@ package android.databinding.layouts;
 
 import android.databinding.BindingBuildInfo;
 
-@BindingBuildInfo(buildId="d2a144d0-8b2d-4b85-b84f-f3ad9de71b0f", modulePackage="com.cnksi.sjjc", sdkRoot="D:\\eclipse\\sdk",layoutInfoDir="D:\\sjjc_android\\adaptionsjjc\\build\\intermediates\\data-binding-info\\debug",exportClassListTo="",isLibrary=false,minSdk=18,enableDebugLogs=false,printEncodedError=true)
+@BindingBuildInfo(buildId="90e41371-da7a-45d7-b789-2554723b2a34", modulePackage="com.cnksi.sjjc", sdkRoot="D:\\eclipse\\sdk",layoutInfoDir="F:\\sjjc_android\\adaptionsjjc\\build\\intermediates\\data-binding-info\\debug",exportClassListTo="",isLibrary=false,minSdk=18,enableDebugLogs=false,printEncodedError=true)
 public class DataBindingInfo {}
--- adaptionsjjc/build/generated/source/dataBinding/release/android/databinding/layouts/DataBindingInfo.java
+++ adaptionsjjc/build/generated/source/dataBinding/release/android/databinding/layouts/DataBindingInfo.java
@@ -2,5 +2,5 @@ package android.databinding.layouts;
 
 import android.databinding.BindingBuildInfo;
 
-@BindingBuildInfo(buildId="97ac754d-a17f-4df4-98aa-cb1ce8588dd6", modulePackage="com.cnksi.sjjc", sdkRoot="D:\\eclipse\\sdk",layoutInfoDir="D:\\sjjc_android\\adaptionsjjc\\build\\intermediates\\data-binding-info\\release",exportClassListTo="",isLibrary=false,minSdk=18,enableDebugLogs=false,printEncodedError=false)
+@BindingBuildInfo(buildId="f3fd961b-5030-4871-a980-4ebf5b37757e", modulePackage="com.cnksi.sjjc", sdkRoot="D:\\eclipse\\sdk",layoutInfoDir="F:\\sjjc_android\\adaptionsjjc\\build\\intermediates\\data-binding-info\\release",exportClassListTo="",isLibrary=false,minSdk=18,enableDebugLogs=false,printEncodedError=true)
 public class DataBindingInfo {}
