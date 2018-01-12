b841179fac63770e9423940b978ed9e265b547d9
junnjundeyuanwang <515334730@qq.com>
2018-01-12 11:50:48 +0800

    'bdloc复原'

--- bdloc/src/main/java/com/cnksi/bdloc/LocationUtil.java
+++ bdloc/src/main/java/com/cnksi/bdloc/LocationUtil.java
@@ -249,9 +249,7 @@ public class LocationUtil {
         locationClient.unRegisterLocationListener(helper);
         helpers.remove(helper);
         if (helpers.isEmpty()) locationClient.stop();
-        else {
-            cancelAllRequest();
-        }
+
     }
 
 
